8e722a6f1d64466dfc85b878f1fdd962ba097c51
Rashi Smita <rashi.smita2611@gmail.com>
2022-11-28 18:29:28 +0530

    Add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function(req,res)
+{
+    res.end('<h1>Express is running up for Buzz</h1>')
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8000;
-
+const port = 8800;
+// using express router
+app.use('/',require('./routes'));
 app.listen(port,function(err)
 {
     if(err)
